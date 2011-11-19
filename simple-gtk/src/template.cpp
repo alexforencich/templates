e24e701102a9c25151f7fa1a06ab9d1d69f3874a
Alex Forencich <alex@alexforencich.com>
2011-11-18 18:05:40 -0800

    Added simple GTK template

---
+++ simple-gtk/src/template.cpp
@@ -0,0 +1,58 @@
+/************************************************************************/
+/* Template                                                             */
+/*                                                                      */
+/* GTK Template                                                         */
+/*                                                                      */
+/* template.cpp                                                         */
+/*                                                                      */
+/* Alex Forencich <alex@alexforencich.com>                              */
+/*                                                                      */
+/* Copyright (c) 2011 Alex Forencich                                    */
+/*                                                                      */
+/* Permission is hereby granted, free of charge, to any person          */
+/* obtaining a copy of this software and associated documentation       */
+/* files(the "Software"), to deal in the Software without restriction,  */
+/* including without limitation the rights to use, copy, modify, merge, */
+/* publish, distribute, sublicense, and/or sell copies of the Software, */
+/* and to permit persons to whom the Software is furnished to do so,    */
+/* subject to the following conditions:                                 */
+/*                                                                      */
+/* The above copyright notice and this permission notice shall be       */
+/* included in all copies or substantial portions of the Software.      */
+/*                                                                      */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,      */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF   */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                */
+/* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS  */
+/* BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN   */
+/* ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN    */
+/* CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE     */
+/* SOFTWARE.                                                            */
+/*                                                                      */
+/************************************************************************/
+
+#include "template.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+Template::Template()
+{
+        set_title("GTK Template");
+        set_position(Gtk::WIN_POS_CENTER);
+        
+        show_all_children();
+}
+
+
+Template::~Template()
+{
+        
+}
+
+
